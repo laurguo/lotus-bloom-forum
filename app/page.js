71aa2557248a40658eaa03f75816aa61f3f141f2
Tayler Nguyen <tayler.nguyen3306@gmail.comgit config --global user.email tayler.nguyen3306@gmail.comgit config --global init.defaultBranch main>
2025-03-21 11:02:19 -0700

    login page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
 import LoginButton from "./components/login-button";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
-      <LoginButton />
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <Image src="/LBText.jpg" alt="picture" width={202} height={150} />
+          <h1>Welcome!</h1>
+          <LoginButton />
+        </div>
+      </main>
     </div>
   );
 }
