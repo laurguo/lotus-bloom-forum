b3c192f00278a2fa8cc8781847c78fe7a82e8e8b
Albert Gonzales <albie@berkeley.edu>
2025-04-07 22:30:04 -0700

    LotusBloom Header and Public Folder :p

--- app/page.js
+++ app/page.js
@@ -1,11 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import LoginButton from "./components/login-button";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [activeHash, setActiveHash] = useState("");
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      setActiveHash(window.location.hash);
+    };
+
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
-      <LoginButton />
+      <main className={styles.main}>
+        : (
+        <div className={styles.container}>
+          <h2>Welcome!</h2>
+          <LoginButton />
+        </div>
+        )
+      </main>
     </div>
   );
 }
--- app/site-selection/page.js
+++ app/site-selection/page.js
@@ -1,8 +1,10 @@
+import styles from "./page.module.css";
 import LogoutButton from "../components/logout-button";
-
+import LotusBloomHeader from "../components/lotusbloomheader";
 export default function SiteSelection() {
   return (
     <div>
+      <LotusBloomHeader />
       <h1>Site Selection</h1>
       <LogoutButton />
     </div>
