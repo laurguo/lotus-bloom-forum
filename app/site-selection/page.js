6e084f6022af4eaec1f07533670e04edeb6570ea
Tayler Nguyen <tayler.nguyen3306@gmail.comgit config --global user.email tayler.nguyen3306@gmail.comgit config --global init.defaultBranch main>
2025-04-08 20:25:33 -0700

    tayler site+login

--- app/site-selection/page.js
+++ app/site-selection/page.js
@@ -1,9 +1,31 @@
 import LogoutButton from "../components/logout-button";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function SiteSelection() {
   return (
-    <div>
-      <h1>Site Selection</h1>
+    <div className={styles.page}>
+      <h1 className={styles.title}>Select a Site!</h1>
+      <div className={styles.selectionGrid}>
+        <Link href="/site/san-antonio-family-resource-center">
+          <div className={styles.site}>San Antonio Family Resource Center</div>
+        </Link>
+        <Link href="/site/lotus-bloom-downtown">
+          <div className={styles.site}>Lotus Bloom Downtown</div>
+        </Link>
+        <Link href="/site/room-to-bloom-east-oakland">
+          <div className={styles.site}>Room to Bloom East Oakland</div>
+        </Link>
+        <Link href="/site/lotus-bloom-general">
+          <div className={styles.site}>Lotus Bloom General</div>
+        </Link>
+        <Link href="/site/family-navigation">
+          <div className={styles.site}>Family Navigation</div>
+        </Link>
+        <Link href="/site/another-super-duper-awesome-site">
+          <div className={styles.site}>Another Super Duper Awesome Site</div>
+        </Link>
+      </div>
       <LogoutButton />
     </div>
   );
